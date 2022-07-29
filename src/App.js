c3e3de139cc3ded4c24548589172a119454006a8
Nhan Nguyen <nhan0123777@gmail.com>
2022-07-29 10:25:36 +0700

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/header'
+import './App.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
+ 
+
---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+    return (
+        <>
+
+            <h1>Header</h1>
+            <p>hello word</p>
+            <h2>Sub header</h2>
+        
+        </>
+    )
+}
+
+export default Header
